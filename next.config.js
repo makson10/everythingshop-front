91eac76670d87e9042d66d33bface61e4dce0917
Maks <makson94021@gmail.com>
2023-07-14 23:57:18 +0300

    make fix in next config for deploy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'src')],
     },
-    distDir: 'build',
     pageExtensions: ['page.js', 'page.jsx', 'page.ts', 'page.tsx'],
     images: {
         remotePatterns: [
